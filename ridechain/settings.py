6563dcff2737cf15e64cc966a5be9c147fce0a66
yaw595 <donkoryaw94@gmail.com>
2025-06-18 01:44:22 +0000

    updated gitignore

--- ridechain/settings.py
+++ ridechain/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'ridechain.wsgi.application'
 
 if IS_PRODUCTION:
     DATABASES = {
-        'default': {
+        'mysql': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': env('DB_NAME'),
             'USER': env('DB_USER'),

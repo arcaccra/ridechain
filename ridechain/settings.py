001b49ea79276f6b3a46d25bb1f390307f225d42
yaw595 <donkoryaw94@gmail.com>
2025-06-18 02:32:03 +0000

    updated gitignore

--- ridechain/settings.py
+++ ridechain/settings.py
@@ -108,8 +108,6 @@ if IS_PRODUCTION:
             'PORT': env('DB_PORT'),
             'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-                'use_unicode': True,
             },
         }
     }

02fb44a95c18631501a6cac34c5688ad422f9273
yaw595 <donkoryaw94@gmail.com>
2025-06-20 17:53:14 +0000

    added waitilist designed

--- homepage/views.py
+++ homepage/views.py
@@ -5,8 +5,6 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from accounts.forms import SubscriberForm
 from accounts.models import Subscriber
-import os
-from django.conf import settings
 
 # Create your views here.
 
@@ -16,31 +14,11 @@ class HomePageView(View):
     form_class = SubscriberForm
     success_url = reverse_lazy('home')
 
-    def get_context_data(self, **kwargs):
-        """Prepares context data for rendering."""
-        context = {
-            'form': kwargs.get('form', self.form_class()),
-            'subscribers': Subscriber.objects.all(),
-        }
-
-        image_dir_path = os.path.join(settings.BASE_DIR, 'static', 'images', 'profiles')
-        profile_images = []
-        try:
-            if os.path.exists(image_dir_path):
-                profile_images = [
-                    f'images/profiles/{f}' for f in os.listdir(image_dir_path)
-                    if os.path.isfile(os.path.join(image_dir_path, f)) and not f.startswith('.')
-                ]
-        except Exception:
-            pass  # Handle exceptions gracefully
-
-        context['profile_images'] = profile_images
-        return context
-
     def get(self, request, *args, **kwargs):
         """Handles GET requests to display the subscriber form and list of subscribers."""
-        context = self.get_context_data()
-        return render(request, self.template_name, context)
+        form = self.form_class()
+        subscribers = Subscriber.objects.all()  # Retrieve all subscribers
+        return render(request, self.template_name, {'form': form, 'subscribers': subscribers})
 
     def post(self, request, *args, **kwargs):
         """Handles POST requests to process the subscriber form."""
@@ -49,6 +27,6 @@ class HomePageView(View):
             form.save()
             messages.success(request, 'Subscription successful!')
             return HttpResponseRedirect(self.success_url)
-        
-        context = self.get_context_data(form=form)
-        return render(request, self.template_name, context)
+        else:
+            subscribers = Subscriber.objects.all()
+            return render(request, self.template_name, {'form': form, 'subscribers': subscribers})

614291f0f86ef1bdf62be8b841d0af80d8b23385
yaw595 <donkoryaw94@gmail.com>
2025-07-11 02:48:53 +0000

    added local server

--- ridechain/settings.py
+++ ridechain/settings.py
@@ -159,6 +159,7 @@ CORS_ORIGIN_WHITELIST = [
     "https://app.arcaccra.com",  # Production frontend
     "http://127.0.0.1:3000",  # Local frontend (e.g., React)
     "https://api.paystack.co",  # Paystack API
+    "http://localhost:5173/" # Local development server for frontend
 ]
 
 # Internationalization

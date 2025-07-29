aa3328af986efe7663d888e7f4f811cfe35c059a
yaw595 <donkoryaw94@gmail.com>
2025-07-29 23:22:53 +0000

    fixing cors issues

--- apis/account_apis/views.py
+++ apis/account_apis/views.py
@@ -125,6 +125,7 @@ class UserUpdateView(generics.UpdateAPIView):
 class DriverListView(generics.ListCreateAPIView):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
 
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()

38a627b9283c19bdad4560921145cac2c1ecacaf
yaw595 <donkoryaw94@gmail.com>
2025-08-02 01:03:02 +0000

    csrf issues

--- apis/account_apis/views.py
+++ apis/account_apis/views.py
@@ -8,10 +8,7 @@ from apis.permissions import IsUserOrReadOnly, IsDriverOrReadOnly
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from .serializers import UserSerializer, UserUpdateSerializer, LoginSerializer, LogoutSerializer, DriverSerializer
 from accounts.models import User, Driver
-
 from ..views import EmptySerializer
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
 
 
 # User List
@@ -21,7 +18,6 @@ class UserListView(generics.ListAPIView):
     permission_classes = [permissions.IsAdminUser]
 
 
-@method_decorator(csrf_exempt, name='create')
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer

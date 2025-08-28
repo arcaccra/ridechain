889e68ee5d6d6d87cd20e94019283e667ca88b63
yaw595 <donkoryaw94@gmail.com>
2025-08-28 18:44:37 +0000

    updated ride list apiss

--- apis/ride_apis/serializers.py
+++ apis/ride_apis/serializers.py
@@ -13,8 +13,8 @@ class LocationSerializer(serializers.ModelSerializer):
 
 class RideListSerializer(serializers.ModelSerializer):
     driver = DriverSerializer(read_only=True)
-    pick_up = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all())
-    drop_off = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all())
+    pick_up = LocationSerializer(read_only=True)
+    drop_off = LocationSerializer(read_only=True)
 
     class Meta:
         model = Ride

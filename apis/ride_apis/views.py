bf662eff3587519d7dbff1a2aa7ee9a94f7c2b4e
yaw595 <donkoryaw94@gmail.com>
2025-11-10 01:37:57 +0000

    trying to wake server logs

--- apis/ride_apis/views.py
+++ apis/ride_apis/views.py
@@ -45,13 +45,12 @@ class RideListView(generics.ListCreateAPIView):
       - pick_up_id: Primary key of the pick_up Location
       - drop_off_id: Primary key of the drop_off Location
       - seats_available, price_per_seat, etc.
-      - departure_time
-      - Optional: arrival_time
-      - etc.
     """
     serializer_class = RideListSerializer
 
     def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return RideCreateSerializer
         return RideListSerializer
 
 
